46f794755653b39ba4da7385aa84e2a42d6b00bc
Alfredwooden <coomcs@gmail.com>
2022-07-21 20:17:24 -0300

    feat: initial commit

---
+++ src/app/empty-route/empty-route.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-route',
+  template: 'app-empty-route.html',
+  styles: ['']
+})
+export class EmptyRouteComponent {}
